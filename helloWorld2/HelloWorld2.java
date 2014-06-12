54e7c294846b405591206607e4afd32cb84fb2dc
macanrk <macanrk@yahoo.com>
2014-06-11 21:42:45 -0700

    youtubedemo2
    initial commit

---
+++ helloWorld2/HelloWorld2.java
@@ -0,0 +1,12 @@
+package helloWorld2;
+
+public class HelloWorld2 {
+	
+		public static void main(String[] args) {
+	System.out.println("Welcome to my world");
+	//System.out.println("Welcome");
+	
+	
+	}
+	
+}
